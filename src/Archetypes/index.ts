934b4140889af4a4a2c479abbfc60aa8552a6685
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-01-19 00:02:16 +0100

    tentando exercicio 4

---
+++ src/Archetypes/index.ts
@@ -0,0 +1,9 @@
+import Archetype from './Archetype';
+// import Dwarf from './Dwarf';
+// import Elf from './Elf';
+// import Halfling from './Halfling';
+// import Orc from './Orc';
+
+export default Archetype;
+
+// export { Dwarf, Elf, Halfling, Orc };

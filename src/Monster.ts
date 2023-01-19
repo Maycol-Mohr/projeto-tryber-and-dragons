8d86348bb22d595b49f8e019d47046c9213fe1d9
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-01-19 19:56:10 +0100

    tentando exercicio 12

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,7 @@
 import { SimpleFighter } from './Fighter';
 
 class Monster implements SimpleFighter {
-  constructor(private _lifePoints = 85, private _strength = 63) {
+  constructor(protected _lifePoints = 85, private _strength = 63) {
     this._lifePoints = this.lifePoints;
     this._strength = this.strength;
   }
